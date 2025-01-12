d94085e9b7fa8e9b5cd85ec606b079f22ef8bf03
Akeksandr Rojkov <dir.gefest@gmail.com>
2025-01-12 20:40:09 +0300

    Change new_branch_1 5 line

--- src/main/java/guru/qu/Main.java
+++ src/main/java/guru/qu/Main.java
@@ -2,6 +2,6 @@ package guru.qu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, QA.GURU!");
+        System.out.println("Daddy loves beer!");
     }
 }
