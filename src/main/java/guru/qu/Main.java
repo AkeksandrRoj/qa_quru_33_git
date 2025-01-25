0b9b88724184ebd49e7f930957b61d7b422b39de
Akeksandr Rojkov <dir.gefest@gmail.com>
2025-01-26 00:40:53 +0300

    improved positiveFillPracticeFormTest

--- src/main/java/guru/qu/Main.java
+++
@@ -1,8 +0,0 @@
-package guru.qu;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Daddy loves whiskey!");
-
-    }
-}
--- src/test/java/homework/thirdHomework/FillPracticeForm.java
+++ src/test/java/homework/thirdHomework/FillPracticeForm.java
@@ -20,11 +20,12 @@ public class FillPracticeForm {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
 
 
     @Test
-    public void PositiveFillPracticeForm() {
+    public void positiveFillPracticeFormTest() {
     open("/automation-practice-form");
     executeJavaScript("$('#fixedban').remove()");
     executeJavaScript("$('#footer').remove()");
@@ -32,14 +33,14 @@ public class FillPracticeForm {
         $("#lastName").setValue("Petrov");
         $("#userEmail").setValue("petr@email.gov");
         $("#gender-radio-1").parent().click();
-        $("#userNumber").setValue("77961459595");
+        $("#userNumber").setValue("7796145959");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("November");
         $(".react-datepicker__year-select").selectOptionByValue("1976");
         $(".react-datepicker__day--010").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbies-checkbox-1").parent().click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/some.png"));
+        $("#uploadPicture").uploadFromClasspath("some.png");
         $("#currentAddress").setValue("Something address");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -49,6 +50,15 @@ public class FillPracticeForm {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Alexander"), text("Petrov"), text("petr@email.gov"));
+        $(".table-responsive").shouldHave(text("Alexander"));
+        $(".table-responsive").shouldHave(text("Petrov"));
+        $(".table-responsive").shouldHave(text("petr@email.gov"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("7796145959"));
+        $(".table-responsive").shouldHave(text("10 November,1976"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("some.png"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
     }
 }
